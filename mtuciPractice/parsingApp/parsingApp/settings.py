9426fb318df0ae4e543aeb684c4c27b2b105f551
KseniaLysikova <kseniakhimki@gmail.com>
2024-07-05 20:52:01 +0300

    fixed problems

--- mtuciPractice/parsingApp/parsingApp/settings.py
+++ mtuciPractice/parsingApp/parsingApp/settings.py
@@ -77,14 +77,14 @@ WSGI_APPLICATION = 'parsingApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
+}
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
@@ -93,7 +93,7 @@ DATABASES = {
         'HOST': 'pgdb',
         'PORT': '5432',
     }
-}
+}'''
 
 
 # Password validation
